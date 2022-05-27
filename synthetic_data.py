ccbed60cefa2c09d56ca1510bb50f885b6e1abd3
obfasina <dami.fasina@yale.edu>
2022-05-27 15:17:16 -0700

    Poisson PDE with working output

--- synthetic_data.py
+++ synthetic_data.py
@@ -14,34 +14,69 @@ import matplotlib.pyplot as plt
 
 # Should have 100 10 x 10 meshes where each mesh correson
 
+#Defining eigenfunctions, source, and eigenvalue
+def X(x,m,xvec):
+    return np.sin( ((m+1)*np.pi*x) /len(xvec) )
+
+def Y(y,n,yvec):
+    return np.sin( ((n+1)*np.pi*y) /len(yvec) ) 
+
+def source(x,y):
+    f =(-x**2-y**2)
+    return f
+
+def Evalue(x,y,m,n,xvec,yvec):
+
+    #Defining constant
+    C = -4/ (len(xvec)*len(yvec)*( ((m + 1)**2*np.pi**2/len(xvec)**2) + ((n + 1)**2*np.pi**2/len(yvec)**2) ))
+
+    #summing over space
+    interm = []
+    for i in range(len(xvec)):
+        for j in range(len(yvec)):
+
+            interm.append(source(x,y)*X(x,m,xvec)*Y(y,n,yvec))
+
+    sumint = np.sum(interm)
+
+    return C*sumint
+
+def Solution(x,y,m,n,xvec,yvec):
+
+    return Evalue(x,y,m,n,xvec,yvec)*X(x,m,xvec)*Y(y,n,yvec)
+
+#Testing synthetic data generation
+
 #Define problem variables
 x = np.arange(0,10)
 y = np.arange(0,10)
 ev_m = np.arange(0,10)
 ev_n = np.arange(0,10)
 
-def X(xi,mi):
-    return np.sin( ((mi+1)*np.pi*xi) /10 )
 
-def Y(yi,ni):
-    return np.sin( ((ni+1)*np.pi*yi) /10 ) 
+#Data generation
 
-def source(xi,yi):
-    return -xi**2 -yi**2
+eval_meshlist = []
+for a in range(len(ev_m)):
+    for b in range(len(ev_n)):
 
-def Evalue(xvec,yvec,me,ne):
+        mesh = []
+        for i in range(len(x)):
+            for j in range(len(y)):
+                mesh.append(Solution(x[i],y[j],ev_m[a],ev_n[b],x,y))
 
-    #Defining constant
-    C = -4/ (len(xvec)*len(yvec)*( ((me + 1)**2*np.pi**2/len(xvec)**2) + ((ne + 1)**2*np.pi**2/len(vec)**2) ))
+        eval_meshlist.append(mesh)
+    
+print(len(eval_meshlist[0]))
 
-    #summing over space
-    interm = []
-    for i in range(len(xvec)):
-        for j in range(len(yvec)):
-            interm.append(source(xvec[i],yvec[j])*X(xvec[i],me)*Y(yvec[j],ne))
+test = np.array(eval_meshlist[0])
+squaremesh = test.reshape((10,10))
+plt.imshow(squaremesh)
 
-    sumint = np.sum(interm)
 
-    return C*sumint
+
+
+        
+
 
 
